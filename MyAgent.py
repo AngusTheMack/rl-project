e4c718a29b4c9d7dc39d5cf641c01af54f82139f
mackenzie angus <1106817@host109-02-01.ms.wits.ac.za>
2019-11-16 14:40:27 +0200

    made the agent more robust and not dependent on the evaluation format

--- MyAgent.py
+++ MyAgent.py
@@ -11,11 +11,14 @@ HUMAN_ACTIONS = (3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33)
 NUM_ACTIONS = len(HUMAN_ACTIONS)
 class MyAgent(AbstractAgent):
     def __init__(self, observation_space, action_space):
+        # We have discretised the action space, thus we need to makesure that we do the same for our inputs
         self.action_space = gym.spaces.Discrete(NUM_ACTIONS)
-        self.observation_space = observation_space
+        # We have use PyTorchFrrame, so we need to do the same for our observation space
+        shape = observation_space.shape
+        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(1, shape[0], shape[1]), dtype=np.uint8)
         replay_buffer = ReplayBuffer(int(5e3))
         self.agent = DQNAgent(
-            observation_space,
+            self.observation_space,
             self.action_space,
             replay_buffer,
             use_double_dqn=True,
@@ -23,10 +26,19 @@ class MyAgent(AbstractAgent):
             batch_size=32,
             gamma=0.99,
         )
+        # Set actions
         self.actions = HUMAN_ACTIONS
+        # Load Weights
         self.agent.policy_network.load_state_dict(torch.load("model.pth",map_location=torch.device(device)))
 
     def act(self, observation):
+        """
+        When we get an obs it will be in RGB, we need to convert it to grayscale because we trained in grayscale
+
+        Then, we need to reshape the new image to be of size (1, 84, 84) instead of (84, 84 before passing it to our model)
+        """
+        observation = cv2.cvtColor(observation, cv2.COLOR_RGB2GRAY)
+        shape =  observation.shape
+        observation = observation.reshape(1, shape[0], shape[1])
         action = self.agent.act(observation)
-        # print(action, self.actions[action])
         return self.actions[action]
